6103d04c35e7ac8889bfc8f624bfc1410249ad7d
genosseredcat <namelessemailadress2@gmail.com>
2021-06-01 16:19:14 +0200

    Möchte einen SWITCH verwenden, um die Abhandlung der Menüwahl zu behandeln.

--- main.py
+++ main.py
@@ -1,23 +1,63 @@
 import time
+from re import match
 
 print("\n")
 print("\n")
+print("=")
+time.sleep(0.2)
+print("==")
+time.sleep(0.2)
+print("===")
+time.sleep(0.2)
+print("====")
+time.sleep(0.2)
+print("=======")
+time.sleep(0.2)
+print("==========")
+time.sleep(0.2)
+print("=================")
+time.sleep(0.2)
+print("==============================")
+time.sleep(0.2)
+print("========================================")
+time.sleep(0.2)
+print("========================================================")
+time.sleep(0.2)
+print("==============================================================================")
+time.sleep(0.2)
+print("===========================================================================================================")
 print("  _       __       __           _  __           ____                __                                    ")
-time.sleep(1)
+time.sleep(0.5)
 print(" | |     / /___   / /_   _____ (_)/ /_ ___     / __ ) ____ _ _____ / /__ __  __ ____   ____   ___   _____ ")
-time.sleep(1)
+time.sleep(0.5)
 print(" | | /| / // _ \ / __ \ / ___// // __// _ \   / __  |/ __ `// ___// //_// / / // __ \ / __ \ / _ \ / ___/ ")
-time.sleep(1)
+time.sleep(0.5)
 print(" | |/ |/ //  __// /_/ /(__  )/ // /_ /  __/  / /_/ // /_/ // /__ / ,<  / /_/ // /_/ // /_/ //  __// /     ")
-time.sleep(1)
+time.sleep(0.5)
 print(" |__/|__/ \___//_.___//____//_/ \__/ \___/  /_____/ \__,_/ \___//_/|_| \__,_// .___// .___/ \___//_/      ")
-print("==========================================================================================================")
 print("                                                                           /_/    /_/                     ")
 print("\n")
-time.sleep(1)
 
-time.sleep(1)
-time.sleep(1)
+
+
+#Optione azeige (Menü)
+print("MENÜ")
+print("_________")
+print("(1) FTP CONFIG")
+print("(2) EMAIL CONFIG")
+print("(3) START")
+print("\n")
+menu_choice = input("Pick a Number: \n")
+print("\n You picked: " + menu_choice)
+
+match menu_choice:
+    case 1:
+        print("OK")
+    case 2:
+        print("Not Found")
+    case 3:
+        print("I'm a teapot")
+
 import ftp_connector
 import smtp_connector
 #Importieren führt direkt aus, wenn es nicht eine function ist.
